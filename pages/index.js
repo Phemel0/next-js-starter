6d8075b228f7dcd327db8f04fa5c4ce4e00b7362
Phemelo <phemelo@Greens-iMac.localdomain>
2020-11-10 13:10:08 +0200

    Title-Header

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hi there this is Phemelo a <a href="https://phemelo.co.za">Front-End Web Developer</a>
         </h1>
 
         <p className={styles.description}>
